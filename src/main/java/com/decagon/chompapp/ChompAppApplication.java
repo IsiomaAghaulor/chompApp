21f32d288889bfdadb166861cfc544f101e6c910
User <user@Users-MacBook-Pro.local>
2022-09-03 19:35:55 +0100

    added roles by default

--- src/main/java/com/decagon/chompapp/ChompAppApplication.java
+++ src/main/java/com/decagon/chompapp/ChompAppApplication.java
@@ -1,13 +1,20 @@
 package com.decagon.chompapp;
 
+import com.decagon.chompapp.models.Role;
+import com.decagon.chompapp.repositories.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-public class ChompAppApplication {
+public class ChompAppApplication implements CommandLineRunner {
+
+    @Autowired
+    RoleRepository roleRepository;
 
     @Bean
     public ModelMapper modelMapper() {
@@ -18,4 +25,17 @@ public class ChompAppApplication {
         SpringApplication.run(ChompAppApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception{
+        Role adminRole = new Role();
+        adminRole.setName("ROLE_ADMIN");
+        roleRepository.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setName("ROLE_PREMIUM");
+        roleRepository.save(userRole);
+    }
 }
+
+
